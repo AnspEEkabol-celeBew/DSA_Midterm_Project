7ea4a625b0cef5f1426f1238fa8e63abe3d96c2a
Keemark Estrada <keemarkeatrada@gmail.com>
2025-10-18 23:03:30 +0800

    Initial Commit

--- lib/utility/assets.dart
+++ lib/utility/assets.dart
@@ -15,7 +15,6 @@ import '../segments/segment12.dart';
 import '../segments/segment13.dart';
 import '../segments/segment14.dart';
 import '../segments/segment15.dart';
-import '../segments/tryItEditor.dart';
 
 import 'builder.dart';
 
@@ -75,8 +74,7 @@ ListView drawerPreset(var context) {
       buildListTile(context, const Segment12(), "Dictionaries"),
       buildListTile(context, const Segment13(), "If Else & Match"),
       buildListTile(context, const Segment14(), "For & While Loops"),
-      buildListTile(context, const Segment15(), "Functions"),
-      buildListTile(context, const TryIt(), "TryIt Editor")
+      buildListTile(context, const Segment15(), "Functions")
     ],
   );
 }
